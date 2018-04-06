6d1a56ab31b3a7596b7ac15641d68519698618db
Riley Wells <rileymorganwells@gmail.com>
2018-04-05 23:51:28 -0600

    Added scripts.js and got it working, toyed with jQuery img fadeIn, made index page spacing better

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#img-portrait').fadeIn(3000);
+
+    var copyright = 'Riley Wells © ' + new Date().getFullYear();
+    document.getElementById('copyright').innerHTML = copyright;
+
+});
